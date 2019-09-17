f452ade8e94d5a94a0fcc9a7c7c33afa5c52ce95
Oyvind Marthinsen <oyvinmar@gmail.com>
2019-09-17 11:31:42 +0000

    fix: make slides work on 1024 screens

--- theme.js
+++ theme.js
@@ -3,5 +3,22 @@ import Provider from './Provider';
 
 export default {
   ...dark,
+  css: {
+    ...dark.css,
+    fontSize: '16px',
+    textAlign: 'center',
+    '@media screen and (min-width:56em)': {
+      fontSize: '32px',
+    },
+    '@media screen and (min-width:64em)': {
+      fontSize: '40px',
+    },
+    '@media screen and (min-width:74em)': {
+      fontSize: '48px',
+    },
+    '@media print': {
+      fontSize: '40px',
+    },
+  },
   Provider,
 };
